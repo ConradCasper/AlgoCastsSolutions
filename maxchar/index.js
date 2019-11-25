583849920017a253d7c41000b8dc2c8010e47112
conradcasper22 <cnrdcasper@gmail.com>
2019-11-25 13:38:28 -0500

    first solution

--- maxchar/index.js
+++ maxchar/index.js
@@ -4,3 +4,27 @@
 // --- Examples
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
+
+
+function maxChar(str) {
+    let chars = {};
+
+    for (let char of str){
+        chars[char] = chars[char]++ || 1
+    }
+
+    let count = 0
+
+    for (const char in chars){
+        if (chars[char] > count){
+            count = chars[char]
+        }
+    }
+
+    for (const char in chars){
+        if (chars[char] === count){
+            return char
+        }
+    }
+
+}
