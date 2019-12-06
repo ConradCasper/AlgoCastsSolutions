71298f15a78be718b48a7ab92d12ab12ddd0c5d8
conradcasper22 <cnrdcasper@gmail.com>
2019-12-06 12:35:26 -0500

    second solution

--- anagrams/index.js
+++ anagrams/index.js
@@ -10,27 +10,59 @@
 
 
 function anagrams(stringA, stringB) {
-    const aCharMap = buildCharMap(stringA.replace(/[^\w]/g, "").toLowerCase())
-    const bCharMap = buildCharMap(stringB.replace(/[^\w]/g, "").toLowerCase())
+    return cleanString(stringA) === cleanString(stringB)
+ }
+ 
+ function cleanString(str) {
+     return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+ }
 
-    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
-        return false
-    }
 
-    for (let char in aCharMap){
-        if (aCharMap[char] !== bCharMap[char]){
-            return false
-        }
-    }
-    return true
-}
 
-function buildCharMap(str){
-    let charMap = {}
 
-    for (let char of str){
-        charMap[char] = charMap[char] + 1 || 1
-    }
 
-    return charMap
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function anagrams(stringA, stringB) {
+//     const aCharMap = buildCharMap(stringA.replace(/[^\w]/g, "").toLowerCase())
+//     const bCharMap = buildCharMap(stringB.replace(/[^\w]/g, "").toLowerCase())
+
+//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+//         return false
+//     }
+
+//     for (let char in aCharMap){
+//         if (aCharMap[char] !== bCharMap[char]){
+//             return false
+//         }
+//     }
+//     return true
+// }
+
+// function buildCharMap(str){
+//     let charMap = {}
+
+//     for (let char of str){
+//         charMap[char] = charMap[char] + 1 || 1
+//     }
+
+//     return charMap
+// }
