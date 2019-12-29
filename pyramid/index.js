b0fc955e2ef554944925aa019342b545d36ac52c
Conrad Casper <cnrdcasper@gmail.com>
2019-12-29 17:35:57 -0500

    recursive. step 2

--- pyramid/index.js
+++ pyramid/index.js
@@ -19,6 +19,11 @@ function pyramid(n, row = 0, level = '') {
     if (row === n) {
         return;
     }
+
+    if (level.length === 2 * n - 1){
+        console.log(level)
+        return pyramid(n, row + 1)
+    }
 }
 
 
