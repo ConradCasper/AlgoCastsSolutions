edb685b5312502729e457c5a633bbe170e082d20
Conrad Casper <cnrdcasper@gmail.com>
2019-12-29 18:06:27 -0500

    recursive. step 3. solution

--- pyramid/index.js
+++ pyramid/index.js
@@ -24,6 +24,17 @@ function pyramid(n, row = 0, level = '') {
         console.log(level)
         return pyramid(n, row + 1)
     }
+
+    const midpoint = Math.floor((2 * n - 1) / 2)
+    let add;
+
+    if (midpoint - row <= level.length && midpoint + row >= level.length){
+        add = "#";
+    } else {
+        add = " ";
+    }
+
+    pyramid(n, row, level + add)
 }
 
 
