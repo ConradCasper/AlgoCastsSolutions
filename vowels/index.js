bfeec97656d61846e47e8e0c50dcdf3b1b9388d6
Conrad Casper <cnrdcasper@gmail.com>
2019-12-29 19:49:03 -0500

    solution 1

--- vowels/index.js
+++ vowels/index.js
@@ -8,16 +8,23 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-    let vowels = [];
+    const matches = str.match(/[aeiou]/gi)
 
-    for (char of str.toLowerCase()){
-        if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u'){
-            vowels.push(char)
-        }
-    }
-    if (vowels === []){
-        return 0
-    } else {
-        return vowels.length
-    }
+    return matches ? matches.length : 0;
 }
+
+
+// function vowels(str) {
+//     let vowels = [];
+
+//     for (char of str.toLowerCase()){
+//         if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u'){
+//             vowels.push(char)
+//         }
+//     }
+//     if (vowels === []){
+//         return 0
+//     } else {
+//         return vowels.length
+//     }
+// }
