1ae51afc4bb254d9dfc786ebd22d7ff807115d16
Conrad <cnrdcasper@gmail.com>
2020-05-08 12:11:14 -0400

    maxChar problem solved

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,50 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-
 function maxChar(str) {
-    let charMap = {};
-    let count = 0;
+    const charMap = {};
+    let max = 0;
     let maxChar = '';
-    
-    for (let char of str){
-        charMap[char] = charMap[char]++ || 1
-    }
 
-    
-    for (let char in charMap){
-        if (charMap[char] > count){
-            count = charMap[char]
-            maxChar = char
+    for (let char of str) {
+        if (charMap[char]){
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
         }
     }
 
-    return maxChar
-
+    for (let char in charMap){
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+    return maxChar;
 }
 
-
-// function maxChar(str) {
-//     let chars = {};
-//     let count = 0
-
-    
-//     for (let char of str){
-//         chars[char] = chars[char]++ || 1
-//     }
-
-    
-
-//     for (const char in chars){
-//         if (chars[char] > count){
-//             count = chars[char]
-//         }
-//     }
-
-//     for (const char in chars){
-//         if (chars[char] === count){
-//             return char
-//         }
-//     }
-
-// }
+module.exports = maxChar;
