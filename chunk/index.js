5591a48ea26debf639c33d6d26fba2e7953cc781
Conrad <cnrdcasper@gmail.com>
2020-05-15 09:59:47 -0400

    first attempt - doesn't work lol

--- chunk/index.js
+++ chunk/index.js
@@ -8,38 +8,25 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-
 function chunk(array, size) {
-    
+    let bigArr = [];
+    while (array.length > 0) {
+        if(array.length === 1){
+            let subArr = [];
+            subArr.push(array.shift());
+            bigArr.push(subArr);
+            return bigArr;
+        } else {
+            let subArr = [];
+            while (subArr.length < size) {
+                for (let num of array) {
+                    subArr.push(num)
+                }
+            }
+            bigArr.push(subArr)
+        }
+    }
+    return bigArr;
 }
 
-
-
-// function chunk(array, size) {
-//     const chunked = []
-   
-//     for (let element of array) {
-//         const last = chunked[chunked.length - 1]
-   
-//         if (!last || last.length === size){
-//             chunked.push([element])
-//         } else {
-//             last.push(element)
-//         }
-//     }
-    
-//     return chunked
-// }
-
-
-
-// function chunk(array, size) {
-//     const chunked = []
-//     let index = 0;
-
-//     while (index < array.length) {
-//         chunked.push(array.slice(index, index + size))
-//         index += size
-//     }
-//     return chunked
-// }
+module.exports = chunk;
