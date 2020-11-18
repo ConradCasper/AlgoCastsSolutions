814b9dcad60d085b45d055bce9d82be039b8771a
Conrad <cnrdcasper@gmail.com>
2020-11-18 10:28:49 -0500

    solved

--- reverseint/index.js
+++ reverseint/index.js
@@ -9,7 +9,8 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-  
+  const reversedN = parseInt(n.toString().split('').reverse().join(''));
+  return reversedN * Math.sign(n);
 }
 
 module.exports = reverseInt;
